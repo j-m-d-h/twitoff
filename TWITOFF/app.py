f6dadcf78076106ea51948a79a6f4658aa739471
j-m-d-h@users.noreply.github.com <marcxdawson@gmail.com>
2019-12-17 23:01:33 -0500

    connected to twitter api

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,7 +1,8 @@
 """Code for our app"""
 
-from flask import Flask
-from .models import DB
+from flask import Flask, render_template, request
+from .models import DB, User
+from decouple import config
 
 # make app factory
 
@@ -9,12 +10,23 @@ def create_app():
     app = Flask(__name__)
 
     # add config for database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+
+    # stop tracking modifications warning
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # show the database the app
     DB.init_app(app)
-    
+
     @app.route('/')
     def root():
-        return 'Welcome to Twitoff'
+        users = User.query.all()
+        return render_template('base.html', title = 'Home', users=users)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title = 'Reset', users=[])
+
     return app
