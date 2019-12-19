82375eb7b41bb8fd76a3205d5d94ef5978566359
j-m-d-h@users.noreply.github.com <marcxdawson@gmail.com>
2019-12-18 23:12:21 -0500

    added predict

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -3,6 +3,10 @@
 from flask import Flask, render_template, request
 from .models import DB, User
 from decouple import config
+from .twitter import add_or_update_user
+from .predict import predict_user
+from dotenv import load_dotenv
+load_dotenv()
 
 # make app factory
 
@@ -29,4 +33,32 @@ def create_app():
         DB.create_all()
         return render_template('base.html', title = 'Reset', users=[])
 
+    @app.route('/user',methods=['POST'])
+    @app.route('/user<name>',methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} successfully added".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets,
+        message=message)
+
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1, user2 = sorted([request.values['user1'],
+                                request.values['user2']])
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves, bozo.'
+        else:
+            prediction = predict_user(user1, user2, request.values['tweet_text'])
+            message = '"{}" is more likely to be said by {} than {}'.format(
+            request.values['tweet_text'], user1 if prediction else user2,
+            user2 if prediction else user1
+            )
+        return render_template('prediction.html', title='Prediction', message=message)
     return app
